7cd8e6e50496c80c509747e213135d8a7b4a9df1
blackbox <dzzirima@gmail.com>
2022-01-10 06:49:47 +0200

    first commit

--- src/index.js
+++ src/index.js
@@ -2,11 +2,29 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App.jsx";
+import { ThirdwebWeb3Provider } from "@3rdweb/hooks";
+
+// addoing the chians to be supported
+// 4  = Rinkeby
+
+const supportedChainIds = [4];
+
+// include waht type of  wallet you want to support
+//in this case , we support MetaMask which is an "injected wallet"
+
+const connectors = {
+  injected: {},
+};
 
 // Render the App component to the DOM
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThirdwebWeb3Provider
+      connectors={connectors}
+      supportedChainIds={supportedChainIds}
+    >
+      <App />
+    </ThirdwebWeb3Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
